d6538da6aa266c9f81057c61affd5e5cc8c6a597
sonicollin98 <sonicollin98@gmail.com>
2026-09-01 16:57:08 +0900

    Created audit quarantine Accounts and Addresses tables

--- scripts/02_data_transformation.sql
+++ scripts/02_data_transformation.sql
@@ -4,9 +4,7 @@ GO
 -- Quarantine bad transactions
 DROP TABLE IF EXISTS audit.QuarantineTransactions;
 
-SELECT * FROM sys.tables WHERE name = 'QuarantineTransactions' AND schema_id = SCHEMA_ID('audit');
-BEGIN
-    CREATE TABLE audit.QuarantineTransactions (
+CREATE TABLE audit.QuarantineTransactions (
         QuarantineID INT IDENTITY(1,1) PRIMARY KEY,
         TransactionID NVARCHAR(200),
         AccountOriginID NVARCHAR(200),
@@ -18,8 +16,7 @@ BEGIN
         Description NVARCHAR(255),
         QuarantineReason NVARCHAR(255),
         QuarantineDate DATETIME DEFAULT GETDATE()
-    );
-END
+);
 GO
 
 -- Move records with missing dates to the Quarantine Table
@@ -51,9 +48,7 @@ GO
 -- Quarantine bad loan records -- 
 DROP TABLE IF EXISTS audit.QuarantineLoans;
 
-SELECT * FROM sys.tables WHERE name = 'QuarantineLoans' AND schema_id = SCHEMA_ID('audit');
-BEGIN
-    CREATE TABLE audit.QuarantineLoans (
+CREATE TABLE audit.QuarantineLoans (
         QuarantineID INT IDENTITY(1,1) PRIMARY KEY,
         LoanID NVARCHAR(200),
         AccountID NVARCHAR(200),
@@ -64,8 +59,7 @@ BEGIN
         EstimatedEndDate NVARCHAR(200),
         QuarantineReason NVARCHAR(255),
         QuarantineDate DATETIME DEFAULT GETDATE()
-    );
-END
+);
 GO
 
 -- Move records with missing dates to the Quarantine Loans Table
@@ -149,9 +143,7 @@ GO
 -- Quarantine bad customer records
 DROP TABLE IF EXISTS audit.QuarantineCustomers;
 
-SELECT * FROM sys.tables WHERE name = 'QuarantineCustomers' AND schema_id = SCHEMA_ID('audit');
-BEGIN
-    CREATE TABLE audit.QuarantineCustomers (
+CREATE TABLE audit.QuarantineCustomers (
         QuarantineID INT IDENTITY(1,1) PRIMARY KEY,
         CustomerID NVARCHAR(200),
         FirstName NVARCHAR(200),
@@ -161,11 +153,10 @@ BEGIN
         CustomerTypeID NVARCHAR(200),
         QuarantineReason NVARCHAR(255),
         QuarantineDate DATETIME DEFAULT GETDATE()
-    );
-END
+);
 GO
 
--- Quarantine Unusable Customer Records
+-- Quarantine Customer Records with Missing Primary Key, Missing Names or Invalid DOB
 INSERT INTO audit.QuarantineCustomers (
     CustomerID, FirstName, LastName, DateOfBirth, AddressID, CustomerTypeID, QuarantineReason
 )
@@ -178,36 +169,204 @@ SELECT
             THEN 'Both First and Last Name are blank'
         WHEN DateOfBirth IS NULL OR TRIM(DateOfBirth) = '' OR TRIM(DateOfBirth) = 'NaT' 
             THEN 'DateOfBirth is missing/NaT'
-        WHEN TRY_CAST(REPLACE(TRIM(DateOfBirth), '.', '-') AS DATE) IS NULL 
+        WHEN TRY_CAST(
+                CASE 
+                -- Check for YYYY-DD-MM pattern (where middle number is a day > 12)
+                WHEN DateOfBirth LIKE '[1-2][0-9][0-9][0-9]-[0-3][0-9]-[0-1][0-9]'
+                AND CAST(SUBSTRING(DateOfBirth, 6, 2) AS INT) > 12
+                    THEN CONCAT(
+                        SUBSTRING(DateOfBirth, 1, 4), '-', 
+                        SUBSTRING(DateOfBirth, 9, 2), '-', 
+                        SUBSTRING(DateOfBirth, 6, 2)
+                    )
+                -- Fallback for standard formatting (dots to dashes, 'NaT' handling)
+                ELSE DateOfBirth
+                END AS DATE) IS NULL
             THEN 'Data Format Violation: Unparseable DateOfBirth string'
-        WHEN TRY_CAST(REPLACE(TRIM(DateOfBirth), '.', '-') AS DATE) > GETDATE() 
+        WHEN TRY_CAST(DateOfBirth AS DATE) > GETDATE() 
             THEN 'Logical Business Violation: Future DateOfBirth detected'
     END AS QuarantineReason
 FROM staging.RawCustomers
 WHERE CustomerID IS NULL OR TRIM(CustomerID) = ''
    OR ((FirstName IS NULL OR TRIM(FirstName) = '') AND (LastName IS NULL OR TRIM(LastName) = ''))
    OR DateOfBirth IS NULL OR TRIM(DateOfBirth) = '' OR TRIM(DateOfBirth) = 'NaT'
-   OR TRY_CAST(REPLACE(TRIM(DateOfBirth), '.', '-') AS DATE) IS NULL
-   OR TRY_CAST(REPLACE(TRIM(DateOfBirth), '.', '-') AS DATE) > GETDATE();
+   OR TRY_CAST(
+                CASE 
+                -- Check for YYYY-DD-MM pattern (where middle number is a day > 12)
+                WHEN DateOfBirth LIKE '[1-2][0-9][0-9][0-9]-[0-3][0-9]-[0-1][0-9]'
+                AND CAST(SUBSTRING(DateOfBirth, 6, 2) AS INT) > 12
+                    THEN CONCAT(
+                        SUBSTRING(DateOfBirth, 1, 4), '-', 
+                        SUBSTRING(DateOfBirth, 9, 2), '-', 
+                        SUBSTRING(DateOfBirth, 6, 2)
+                    )
+                -- Fallback for standard formatting (dots to dashes, 'NaT' handling)
+                ELSE DateOfBirth
+                END AS DATE) IS NULL
+   OR TRY_CAST(CASE 
+                -- Check for YYYY-DD-MM pattern (where middle number is a day > 12)
+                WHEN DateOfBirth LIKE '[1-2][0-9][0-9][0-9]-[0-3][0-9]-[0-1][0-9]'
+                AND CAST(SUBSTRING(DateOfBirth, 6, 2) AS INT) > 12
+                    THEN CONCAT(
+                        SUBSTRING(DateOfBirth, 1, 4), '-', 
+                        SUBSTRING(DateOfBirth, 9, 2), '-', 
+                        SUBSTRING(DateOfBirth, 6, 2)
+                    )
+                -- Fallback for standard formatting (dots to dashes, 'NaT' handling)
+                ELSE DateOfBirth
+                END AS DATE) > GETDATE();
 GO
 
 -- 3. Verification Query: Inspect Cleaned DOB Output
-SELECT 
-    CustomerID,
-    ISNULL(NULLIF(TRIM(FirstName), ''), 'Unknown') AS FirstName,
-    ISNULL(NULLIF(TRIM(LastName), ''), 'Unknown') AS LastName,
-    -- Pre-clean strings ('NaT' to NULL, '.' to '-') then cast cleanly to DATE
-    TRY_CAST(
-        REPLACE(
-            NULLIF(TRIM(DateOfBirth), 'NaT'), 
-            '.', '-'
-        ) AS DATE
-    ) AS CleanDateOfBirth
-FROM staging.RawCustomers
-WHERE CustomerID NOT IN (SELECT CustomerID FROM audit.QuarantineCustomers);
+-- SELECT 
+--     CustomerID,
+--     ISNULL(NULLIF(TRIM(FirstName), ''), 'Unknown') AS FirstName,
+--     ISNULL(NULLIF(TRIM(LastName), ''), 'Unknown') AS LastName,
+--     DateOfBirth,
+--     -- Pre-clean strings ('NaT' to NULL, '.' to '-') then cast cleanly to DATE
+--     TRY_CAST(
+--                 CASE 
+--                 -- Check for YYYY-DD-MM pattern (where middle number is a day > 12)
+--                 WHEN DateOfBirth LIKE '[1-2][0-9][0-9][0-9]-[0-3][0-9]-[0-1][0-9]'
+--                 AND CAST(SUBSTRING(DateOfBirth, 6, 2) AS INT) > 12
+--                     THEN CONCAT(
+--                         SUBSTRING(DateOfBirth, 1, 4), '-', 
+--                         SUBSTRING(DateOfBirth, 9, 2), '-', 
+--                         SUBSTRING(DateOfBirth, 6, 2)
+--                     )
+--                 -- Fallback for standard formatting (dots to dashes, 'NaT' handling)
+--                 ELSE DateOfBirth
+--                 END AS DATE) AS CleanDateOfBirth
+-- FROM staging.RawCustomers
+-- WHERE CustomerID NOT IN (SELECT CustomerID FROM audit.QuarantineCustomers)
+-- GO
+
+-- Quarantine Accounts Table --
+DROP TABLE IF EXISTS audit.QuarantineAccounts;
+
+CREATE TABLE audit.QuarantineAccounts (
+        QuarantineID INT IDENTITY(1,1) PRIMARY KEY,
+        AccountID NVARCHAR(200),
+        CustomerID NVARCHAR(200),
+        AccountTypeID NVARCHAR(200),
+        AccountStatusID NVARCHAR(200),
+        Balance NVARCHAR(200),
+        OpeningDate NVARCHAR(200),
+        QuarantineReason NVARCHAR(255),
+        QuarantineDate DATETIME DEFAULT GETDATE()
+);
 GO
 
-SELECT * FROM audit.QuarantineCustomers
-WHERE QuarantineReason != 'Data Format Violation: Unparseable DateOfBirth string';
+-- Move records with missing dates to the Quarantine Table
+INSERT INTO audit.QuarantineAccounts (
+    AccountID,
+    CustomerID,
+    AccountTypeID,
+    AccountStatusID,
+    Balance,
+    OpeningDate,
+    QuarantineReason
+)
+SELECT 
+    AccountID,
+    CustomerID,
+    AccountTypeID,
+    AccountStatusID,
+    Balance,
+    OpeningDate,
+    CASE 
+        WHEN AccountID IS NULL OR TRIM(AccountID) = '' 
+            THEN 'Missing Primary Key: AccountID is NULL or Blank'
+        WHEN OpeningDate IS NULL OR TRIM(OpeningDate) = '' 
+            THEN 'OpeningDate is missing'
+        WHEN TRY_CAST(
+                CASE 
+                -- Check for YYYY-DD-MM pattern (where middle number is a day > 12)
+                WHEN OpeningDate LIKE '[1-2][0-9][0-9][0-9]-[0-3][0-9]-[0-1][0-9]'
+                AND CAST(SUBSTRING(OpeningDate, 6, 2) AS INT) > 12
+                    THEN CONCAT(
+                        SUBSTRING(OpeningDate, 1, 4), '-', 
+                        SUBSTRING(OpeningDate, 9, 2), '-', 
+                        SUBSTRING(OpeningDate, 6, 2)
+                    )
+                -- Fallback for standard formatting (dots to dashes, 'NaT' handling)
+                ELSE OpeningDate
+                END AS DATE) IS NULL
+            THEN 'Data Format Violation: Unparseable OpeningDate string'
+        WHEN TRY_CAST(OpeningDate AS DATE) > GETDATE() 
+            THEN 'Logical Business Violation: Future OpeningDate detected'
+    END AS QuarantineReason
+FROM staging.RawAccounts
+WHERE AccountID IS NULL OR TRIM(AccountID) = ''
+    OR OpeningDate IS NULL OR TRIM(OpeningDate) = ''
+    OR TRY_CAST(
+                CASE 
+                -- Check for YYYY-DD-MM pattern (where middle number is a day > 12)
+                WHEN OpeningDate LIKE '[1-2][0-9][0-9][0-9]-[0-3][0-9]-[0-1][0-9]'
+                AND CAST(SUBSTRING(OpeningDate, 6, 2) AS INT) > 12
+                    THEN CONCAT(
+                        SUBSTRING(OpeningDate, 1, 4), '-', 
+                        SUBSTRING(OpeningDate, 9, 2), '-', 
+                        SUBSTRING(OpeningDate, 6, 2)
+                    )
+                -- Fallback for standard formatting (dots to dashes, 'NaT' handling)
+                ELSE OpeningDate
+                END AS DATE) IS NULL
+    OR TRY_CAST(CASE 
+                -- Check for YYYY-DD-MM pattern (where middle number is a day > 12)
+                WHEN OpeningDate LIKE '[1-2][0-9][0-9][0-9]-[0-3][0-9]-[0-1][0-9]'
+                AND CAST(SUBSTRING(OpeningDate, 6, 2) AS INT) > 12
+                    THEN CONCAT(
+                        SUBSTRING(OpeningDate, 1, 4), '-', 
+                        SUBSTRING(OpeningDate, 9, 2), '-', 
+                        SUBSTRING(OpeningDate, 6, 2)
+                    )
+                -- Fallback for standard formatting (dots to dashes, 'NaT' handling)
+                ELSE OpeningDate
+                END AS DATE) > GETDATE();
+GO
+
+-- Quarantine Addresses Table -- 
+DROP TABLE IF EXISTS audit.QuarantineAddresses;
+
+CREATE TABLE audit.QuarantineAddresses (
+        QuarantineID INT IDENTITY(1,1) PRIMARY KEY,
+        AddressID NVARCHAR(200),
+        Street NVARCHAR(200),
+        City NVARCHAR(200),
+        Country NVARCHAR(200),
+        QuarantineReason NVARCHAR(255),
+        QuarantineDate DATETIME DEFAULT GETDATE()
+);
+GO
+
+INSERT INTO audit.QuarantineAddresses (
+    AddressID,
+    Street,
+    City,
+    Country,
+    QuarantineReason
+)
+SELECT 
+    AddressID,
+    Street,
+    City,
+    Country,
+    CASE 
+        WHEN AddressID IS NULL OR TRIM(AddressID) = '' 
+            THEN 'Missing Primary Key: AddressID is NULL or Blank'
+        WHEN Street IS NULL OR TRIM(Street) = '' 
+            THEN 'Missing Street'
+        WHEN City IS NULL OR TRIM(City) = '' 
+            THEN 'Missing City'
+        WHEN Country IS NULL OR TRIM(Country) = '' 
+            THEN 'Missing Country'
+    END AS QuarantineReason
+FROM staging.RawAddresses
+WHERE AddressID IS NULL OR TRIM(AddressID) = ''
+    OR Street IS NULL OR TRIM(Street) = ''
+    OR City IS NULL OR TRIM(City) = ''
+    OR Country IS NULL OR TRIM(Country) = '';
+GO
 
 
--- scripts/data_auditing.sql
+++ scripts/data_auditing.sql
@@ -2,35 +2,36 @@ USE CapitalMarketsDB;
 GO 
 
 
-SELECT 'Transaction Types'
-SELECT * FROM staging.RawTransactionTypes;
-SELECT 'Transactions'
-SELECT TOP 5 * FROM staging.RawTransactions;
-SELECT 'Loan Statuses'
-SELECT * FROM staging.RawLoanStatuses;
-SELECT 'Loans'
-SELECT TOP 5 * FROM staging.RawLoans;
-SELECT 'Account Statuses'
-SELECT * FROM staging.RawAccountStatuses;
-SELECT 'Account Types'
-SELECT * FROM staging.RawAccountTypes;
-SELECT 'Accounts'
-SELECT TOP 5 * FROM staging.RawAccounts;
-SELECT 'Customer Types'
-SELECT * FROM staging.RawCustomerTypes;
-SELECT 'Customers'
-SELECT TOP 5 * FROM staging.RawCustomers;
-SELECT 'Branches'
-SELECT TOP 5 * FROM staging.RawBranches;
-SELECT 'Addresses'
-SELECT TOP 5 * FROM staging.RawAddresses;
+-- SELECT 'Transaction Types'
+-- SELECT * FROM staging.RawTransactionTypes;
+-- SELECT 'Transactions'
+-- SELECT TOP 5 * FROM staging.RawTransactions;
+-- SELECT 'Loan Statuses'
+-- SELECT * FROM staging.RawLoanStatuses;
+-- SELECT 'Loans'
+-- SELECT TOP 5 * FROM staging.RawLoans;
+-- SELECT 'Account Statuses'
+-- SELECT * FROM staging.RawAccountStatuses;
+-- SELECT 'Account Types'
+-- SELECT * FROM staging.RawAccountTypes;
+-- SELECT 'Accounts'
+-- SELECT TOP 5 * FROM staging.RawAccounts;
+-- SELECT 'Customer Types'
+-- SELECT * FROM staging.RawCustomerTypes;
+-- SELECT 'Customers'
+-- SELECT TOP 5 * FROM staging.RawCustomers;
+-- SELECT 'Branches'
+-- SELECT TOP 5 * FROM staging.RawBranches;
+-- SELECT 'Addresses'
+-- SELECT TOP 5 * FROM staging.RawAddresses;
 
-SELECT 
-    LoanID,
-    TRY_CAST(PrincipalAmount AS DECIMAL(18,2)) AS CleanAmount
-FROM staging.RawLoans
-WHERE TRY_CAST(PrincipalAmount AS DECIMAL(18,2)) IS NULL 
-  AND PrincipalAmount IS NOT NULL; -- Pinpoints unparseable strings
+
+SELECT DISTINCT CustomerID FROM staging.RawCustomers
+GROUP BY CustomerID
+HAVING COUNT(*) > 1; -- Identifies duplicate CustomerIDs
+
+SELECT * FROM staging.RawCustomers
+WHERE CustomerID = 10056; -- Example of a duplicate CustomerID
 
 
 
